b74b05a9ae90e849027007a36b94818e337464f0
costa <c.diazs.2019@alumnos.urjc.es>
2020-02-26 13:40:56 +0100

    Session 8. Exercise 4.

--- Session 8/server.py
+++ Session 8/server.py
@@ -1,5 +1,43 @@
+##Modified from the Session 8 class.
+
 import socket
 
-PORT = '8080'
+PORT = 8080
 IP = '212.128.253.147'
+MAX_OPEN_REQUESTS = 50
+number_con = 0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    s.listen(MAX_OPEN_REQUESTS)
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = s.accept()
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client, if any
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Message from client: {}".format(msg))
+
+        # Send the messag
+        message = "Hello from the teacher's server"
+        send_bytes = str.encode(message)
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    s.close()
