83d36362e5d917aafbd32573ab9ea6a32a5b9243
costa <c.diazs.2019@alumnos.urjc.es>
2020-02-26 12:49:02 +0100

    Session 8-bis.

---
+++ Session 8-bis/client1.py
@@ -0,0 +1,23 @@
+import socket
+
+IP = '212.128.253.128'
+PORT = 8080
+
+# First, create the socket
+# We will always use this parameters: AF_INET y SOCK_STREAM
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
+
+# Send data. No strings can be send, only bytes
+# It necesary to encode the string into bytes
+s.send(str.encode("Viva Vox"))
+
+msg = s.recv(2000)
+print("MESSAGE FROM THE SERVER:\n")
+print(msg.decode("utf-8"))
+
+
+# Closing the socket
+s.close()
