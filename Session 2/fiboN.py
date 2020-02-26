61d0899c201709262788d78b6e10dcaee4235deb
costita <costadiazsanchez@gmail.com>
2020-02-26 11:23:54 +0100

    Session 2. Exercise 2.

---
+++ Session 2/fiboN.py
@@ -0,0 +1,16 @@
+def fib (n):
+    x = 0
+    y = 1
+    sum = 0
+    count = 0
+
+    while count < n:
+        aux = x + y
+        x = y
+        y = aux
+        count = count + 1
+    return (x)
+
+print ("5th Fibonacci term: ", fib (5))
+print ("10th FIbonacci term: ", fib (10))
+print ("15th Fibonacci term: ", fib (15))
