296f904b5901502582ea027627b7a6640f2b25c8
costa <c.diazs.2019@alumnos.urjc.es>
2020-02-26 12:43:02 +0100

    P1. Exercise 4.

---
+++ P1/Ex4.py
@@ -0,0 +1,11 @@
+from Seq1 import Seq
+
+print("----Exercise 4----")
+
+s1 = Seq("")
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print("Sequence 1", ": (Lenght: ", s1.len(), ")", s1)
+print("Sequence 2", ": (Lenght: ", s2.len(), ")", s2)
+print("Sequence 3", ": (Lenght: ", s3.len(), ")", s3)
