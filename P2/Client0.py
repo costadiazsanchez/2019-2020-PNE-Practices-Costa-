e1cb5aab4cb93c038a056c7181beb1691d94e153
costa <c.diazs.2019@alumnos.urjc.es>
2020-02-26 18:41:26 +0100

    P2. CLient0.

--- P2/Client0.py
+++ P2/Client0.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import socket
+import termcolor
 
 class Client:
     def __init__(self, ip, port):
@@ -7,6 +9,37 @@ class Client:
 
     def ping(self):
         print("OK!")
+
+    def __str__(self):
+        x = f"Connection to SERVER at {self.ip}, PORT: {self.port}"
+        return x
+
+    def talk(self, msg):
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.ip, self.port))
+
+        # Send data.
+        s.send(str.encode(msg))
+
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+
+        # Close the socket
+        s.close()
+
+        # Return the response
+        return response
+
+    def debug_talk(self, message):
+        msg = self.talk(message)
+        print("To server: ")
+        termcolor.cprint(message, 'red')
+        print("From server: ")
+        termcolor.cprint(msg, 'yellow')
+
 
 
 
