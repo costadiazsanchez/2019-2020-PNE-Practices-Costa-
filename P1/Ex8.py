4b96f8b724b70833988cf66829498f8cf28df062
costa <c.diazs.2019@alumnos.urjc.es>
2020-02-26 12:43:49 +0100

    P1. Exercise 8.

---
+++ P1/Ex8.py
@@ -0,0 +1,21 @@
+from Seq1 import Seq
+
+print("----Exercise 8----")
+bases = ["A", "C", "G", "T"]
+
+s1 = Seq("")
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print("Sequence 1", ": (Lenght: ", s1.len(), ")", s1)
+print("Bases: ", s1.count())
+print("Reverse: ", s1.reverse())
+print("Complement:", s1.complement())
+print("Sequence 2", ": (Lenght: ", s2.len(), ")", s2)
+print("Bases: ", s2.count())
+print("Reverse: ", s2.reverse())
+print("Complement:", s2.complement())
+print("Sequence 3", ": (Lenght: ", s3.len(), ")", s3)
+print("Bases: ", s3.count())
+print("Reverse: ", s3.reverse())
+print("Complement:", s3.complement())
