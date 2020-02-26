ca7f6ed7d8edebb5c8c210f61371a5b5157a5144
costa <c.diazs.2019@alumnos.urjc.es>
2020-02-26 18:41:07 +0100

    P2. Exercise 4.

---
+++ P2/Ex4.py
@@ -0,0 +1,15 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 4
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+IP = "212.128.253.130"
+PORT = 8080
+
+c = Client(IP, PORT)
+print(c)
+
+c.debug_talk("Message 1---")
+c.debug_talk("Message 2: Testing !!!")
