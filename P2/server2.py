dbb5a3d981b9f999c138cd22d1480e34b2617938
costita <costadiazsanchez@gmail.com>
2020-03-02 10:26:24 +0100

    P2. Server 2.

---
+++ P2/server2.py
@@ -0,0 +1,43 @@
+import socket
+import termcolor
+
+PORT = 8081
+IP = '192.168.0.13'
+MAX_OPEN_REQUESTS = 50
+number_con = 0
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    s.listen(MAX_OPEN_REQUESTS)
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = s.accept()
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client, if any
+        msg = clientsocket.recv(2048).decode('utf-8')
+        print("Message from client: ")
+        termcolor.cprint(format(msg), 'blue')
+
+        # Send the message
+        message = "Hello from the teacher's server"
+        send_bytes = str.encode(message)
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    s.close()
