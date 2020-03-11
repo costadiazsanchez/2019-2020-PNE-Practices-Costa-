7be36b8587cd0e36fb4475ec59e9bb53a90e1b61
costita <costadiazsanchez@gmail.com>
2020-03-11 10:22:51 +0100

    Session 12. Exercise 1.

---
+++ Session 12/Exercise 1.py
@@ -0,0 +1,3 @@
+#1. The request line is: GET /hello HTTP/1.1 and the Path is hello HTTP
+#2. The request line is: GET /file.html HTTP/1.1 and the Path i sfile.html HTTP
+#3. The request line is: GET /hi/there?name=virus&type=corona HTTP/1.1 and the Path is hi/there?name=virus&type=corona HTTP
