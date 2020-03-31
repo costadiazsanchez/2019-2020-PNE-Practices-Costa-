c8ccc19b4ce0bef3ee18bcd5a70ea72307527f03
costita <costadiazsanchez@gmail.com>
2020-03-31 18:50:11 +0200

    Session 18. Exercise 1.

---
+++ Session 18/test-json-EX01.py
@@ -0,0 +1,34 @@
+import json
+import termcolor
+from pathlib import Path
+
+# -- Read the json file
+jsonstring = Path("people-EX01.json").read_text()
+
+# Create the object person from the json string
+person = json.loads(jsonstring)
+print("Total lenght of people: ", len(person))
+
+for element in person:
+
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(element['Firstname'], element['Lastname'])
+    termcolor.cprint("Age: ", 'green', end="")
+    print(element['age'])
+
+    # Get the phoneNumber list
+    phoneNumbers = element['phoneNumber']
+
+    # Print the number of elements in the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("Phone {}:".format(i), 'blue')
+
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("Number: ", 'red', end='')
+        print(num['number'])
