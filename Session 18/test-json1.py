6ff72743d4714e1e76a12974aba10d4b4026aeef
costita <costadiazsanchez@gmail.com>
2020-03-31 18:08:22 +0200

    Session 18. Example.

--- Session 18/test-json1.py
+++ Session 18/test-json1.py
@@ -8,8 +8,8 @@ jsonstring = Path("people-1.json").read_text()
 # Create the object person from the json string
 person = json.loads(jsonstring)
 
-# Person is now a dictionary. We can read the values
-# associated to the fields 'Firstname', 'Lastname' and 'age'
+# Person is now a dictionary. We can read the values.
+# associated to the fields 'Firstname', 'Lastname' and 'age'.
 
 # -- Read the Firtname
 firstname = person['Firstname']
