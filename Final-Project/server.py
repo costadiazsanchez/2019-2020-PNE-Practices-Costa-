0d91c94f2c2659520ac6a48037df9a4175e62000
costita <costadiazsanchez@gmail.com>
2020-04-22 18:22:50 +0200

    FINAL PROJECT SERVER

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -151,130 +151,133 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                                       </html>'''
                         code = 200
 
-                    elif first_w in '/geneSeq':  # /sequence/id/ENSG00000157764?content-type=text/plain
-                        auxiliar_endpoint = 'xrefs/symbol/homo_sapiens/'
-                        after_int = command2[1]
-                        w1, w2 = after_int.split('=')
-                        conn.request("GET", auxiliar_endpoint + w2 + parameters)
-                        r1 = conn.getresponse()
-                        data1 = r1.read().decode("utf-8")
-                        auxiliar_console = json.loads(data1)
-                        identificator = auxiliar_console[0]
-                        id_gene = identificator['id']
+                    try:
 
-                        endpoint = 'sequence/id/'
-                        conn.request("GET", endpoint + id_gene + parameters)
-                        r2 = conn.getresponse()
-                        data2 = r2.read().decode("utf-8")
-                        console = json.loads(data2)
+                        if first_w in '/geneSeq':  # /sequence/id/ENSG00000157764?content-type=text/plain
+                            auxiliar_endpoint = 'xrefs/symbol/homo_sapiens/'
+                            after_int = command2[1]
+                            w1, w2 = after_int.split('=')
+                            conn.request("GET", auxiliar_endpoint + w2 + parameters)
+                            r1 = conn.getresponse()
+                            data1 = r1.read().decode("utf-8")
+                            auxiliar_console = json.loads(data1)
+                            identificator = auxiliar_console[0]
+                            id_gene = identificator['id']
+
+                            endpoint = 'sequence/id/'
+                            conn.request("GET", endpoint + id_gene + parameters)
+                            r2 = conn.getresponse()
+                            data2 = r2.read().decode("utf-8")
+                            console = json.loads(data2)
 
 
-                        output = f'''<!DOCTYPE html>
-                               <html lang = "en">            
-                               <head>  
-                               <meta charset = "utf-8"
-                                 <title> SEQUENCE GENE </title>
-                                 </head>
-                                 <body style="background-color: lightblue;">       
-                              '''
-                        sequence = f'{console["seq"]}'
-                        output += f'<p> The sequence of gene {w2} is:  </p>'
-                        output += f'<textarea rows = "100" "cols = 100"> {sequence} </textarea>'
-                        output += '''<a href="/">Main page</a>
-                                      </body>
-                                      </html>'''
+                            output = f'''<!DOCTYPE html>
+                                   <html lang = "en">            
+                                   <head>  
+                                   <meta charset = "utf-8"
+                                     <title> SEQUENCE GENE </title>
+                                     </head>
+                                     <body style="background-color: lightblue;">       
+                                  '''
+                            sequence = f'{console["seq"]}'
+                            output += f'<p> The sequence of gene {w2} is:  </p>'
+                            output += f'<textarea rows = "100" "cols = 100"> {sequence} </textarea>'
+                            output += '''<a href="/">Main page</a>
+                                          </body>
+                                          </html>'''
 
-                    elif first_w in '/geneInfo':  # /lookup/id/ENSG00000157764?content-type=text/plain
-                        auxiliar_endpoint = 'xrefs/symbol/homo_sapiens/'
-                        after_int = command2[1]
-                        w1, w2 = after_int.split('=')
-                        conn.request("GET", auxiliar_endpoint + w2 + parameters)
-                        r1 = conn.getresponse()
-                        data1 = r1.read().decode("utf-8")
-                        auxiliar_console = json.loads(data1)
-                        identificator = auxiliar_console[0]
-                        id_gene = identificator['id']
+                        elif first_w in '/geneInfo':  # /lookup/id/ENSG00000157764?content-type=text/plain
+                            auxiliar_endpoint = 'xrefs/symbol/homo_sapiens/'
+                            after_int = command2[1]
+                            w1, w2 = after_int.split('=')
+                            conn.request("GET", auxiliar_endpoint + w2 + parameters)
+                            r1 = conn.getresponse()
+                            data1 = r1.read().decode("utf-8")
+                            auxiliar_console = json.loads(data1)
+                            identificator = auxiliar_console[0]
+                            id_gene = identificator['id']
 
-                        endpoint = 'lookup/id/'
-                        conn.request("GET", endpoint + id_gene + parameters)
-                        r2 = conn.getresponse()
-                        data2 = r2.read().decode("utf-8")
-                        console = json.loads(data2)
+                            endpoint = 'lookup/id/'
+                            conn.request("GET", endpoint + id_gene + parameters)
+                            r2 = conn.getresponse()
+                            data2 = r2.read().decode("utf-8")
+                            console = json.loads(data2)
 
-                        output = f'''<!DOCTYPE html>
-                              <html lang = "en">            
-                              <head>  
-                              <meta charset = "utf-8"
-                                <title> INFO ABOUT GENE {w2} </title>
-                                </head>
-                                <body style="background-color: lightblue;"> '''
-                        output += f'<p> The gene is on chromosome {console["seq_region_name"]}</p>'
-                        output += f'<p> The identificator is {id_gene}</p>'
-                        output += f'<p> The gene start on position {console["start"]}</p>'
-                        output += f'<p> The gene ends on position {console["end"]}</p>'
-                        output += f'<p> The length of the gene is {console["end"] - console["start"]} </p>'
-                        output += '''<a href="/">Main page</a>
-                                     </body>
-                                     </html>'''
-
-                    elif first_w in '/geneCalc':  # /sequence/id/ENSG00000157764?content-type=text/plain
-                        auxiliar_endpoint = 'xrefs/symbol/homo_sapiens/'
-                        after_int = command2[1]
-                        w1, w2 = after_int.split('=')
-                        conn.request("GET", auxiliar_endpoint + w2 + parameters)
-                        r1 = conn.getresponse()
-                        data1 = r1.read().decode("utf-8")
-                        auxiliar_console = json.loads(data1)
-                        identificator = auxiliar_console[0]
-                        id_gene = identificator['id']
-
-                        endpoint = 'sequence/id/'
-                        conn.request("GET", endpoint + id_gene + parameters)
-                        r2 = conn.getresponse()
-                        data2 = r2.read().decode("utf-8")
-                        console = json.loads(data2)
-                        sequence = Seq(console["seq"])
-
-                        output = f'''<!DOCTYPE html>
+                            output = f'''<!DOCTYPE html>
                                   <html lang = "en">            
                                   <head>  
                                   <meta charset = "utf-8"
-                                    <title> CALCULATIONS ABOUT GENE {w2} </title>
+                                    <title> INFO ABOUT GENE {w2} </title>
                                     </head>
                                     <body style="background-color: lightblue;"> '''
-                        output += f'<p> The length of the gene is {sequence.len()}</p>'
-                        for element in BASES:
-                            output += f'\n{element}: {sequence.count_base(element)} ({sequence.count_base(element) * 100 / sequence.len()}%)'
-                        output += '''\n<a href="/">Main page</a>
-                                     </body>
-                                     </html>'''
+                            output += f'<p> The gene is on chromosome {console["seq_region_name"]}</p>'
+                            output += f'<p> The identificator is {id_gene}</p>'
+                            output += f'<p> The gene start on position {console["start"]}</p>'
+                            output += f'<p> The gene ends on position {console["end"]}</p>'
+                            output += f'<p> The length of the gene is {console["end"] - console["start"]} </p>'
+                            output += '''<a href="/">Main page</a>
+                                         </body>
+                                         </html>'''
 
-                    elif first_w in '/geneList':
-                        endpoint = 'overlap/region/human/'
-                        after_int = command2[1]
-                        v1, v2, v3 = after_int.split('&')
-                        w1, w2 = v1.split('=')  # w1 = chromo and w2 = value(chromo)
-                        t1, t2 = v2.split("=")  # t1 = start and t2 = value (start)
-                        z1, z2 = v3.split("=")  # z1 = ends and z2 = value (ends)
-                        conn.request("GET",
-                                     endpoint + w2 + ':' + t2 + '-' + z2 + '?feature=gene;content-type=application/json')
-                        r1 = conn.getresponse()
-                        data1 = r1.read().decode("utf-8")
-                        console = json.loads(data1)
-                        output = f'''<!DOCTYPE html>
-                                  <html lang = "en">            
-                                  <head>  
-                                  <meta charset = "utf-8"
-                                    <title> GENE LIST OF THE CHROMOSOME {w2} that starts at {t2} and ends at {z2} </title>
-                                    </head>
-                                    <body style="background-color: lightblue;"> '''
-                        for element in console:
-                            output += f'<p> - {element["external_name"]}</p>'
+                        elif first_w in '/geneCalc':  # /sequence/id/ENSG00000157764?content-type=text/plain
+                            auxiliar_endpoint = 'xrefs/symbol/homo_sapiens/'
+                            after_int = command2[1]
+                            w1, w2 = after_int.split('=')
+                            conn.request("GET", auxiliar_endpoint + w2 + parameters)
+                            r1 = conn.getresponse()
+                            data1 = r1.read().decode("utf-8")
+                            auxiliar_console = json.loads(data1)
+                            identificator = auxiliar_console[0]
+                            id_gene = identificator['id']
 
-                        output += '''<a href="/">Main page</a>
-                                     </body>
-                                     </html>'''
+                            endpoint = 'sequence/id/'
+                            conn.request("GET", endpoint + id_gene + parameters)
+                            r2 = conn.getresponse()
+                            data2 = r2.read().decode("utf-8")
+                            console = json.loads(data2)
+                            sequence = Seq(console["seq"])
 
+                            output = f'''<!DOCTYPE html>
+                                      <html lang = "en">            
+                                      <head>  
+                                      <meta charset = "utf-8"
+                                        <title> CALCULATIONS ABOUT GENE {w2} </title>
+                                        </head>
+                                        <body style="background-color: lightblue;"> '''
+                            output += f'<p> The length of the gene is {sequence.len()}</p>'
+                            for element in BASES:
+                                output += f'\n{element}: {sequence.count_base(element)} ({sequence.count_base(element) * 100 / sequence.len()}%)'
+                            output += '''\n<a href="/">Main page</a>
+                                         </body>
+                                         </html>'''
+
+                        elif first_w in '/geneList':
+                            endpoint = 'overlap/region/human/'
+                            after_int = command2[1]
+                            v1, v2, v3 = after_int.split('&')
+                            w1, w2 = v1.split('=')  # w1 = chromo and w2 = value(chromo)
+                            t1, t2 = v2.split("=")  # t1 = start and t2 = value (start)
+                            z1, z2 = v3.split("=")  # z1 = ends and z2 = value (ends)
+                            conn.request("GET",
+                                         endpoint + w2 + ':' + t2 + '-' + z2 + '?feature=gene;content-type=application/json')
+                            r1 = conn.getresponse()
+                            data1 = r1.read().decode("utf-8")
+                            console = json.loads(data1)
+                            output = f'''<!DOCTYPE html>
+                                      <html lang = "en">            
+                                      <head>  
+                                      <meta charset = "utf-8"
+                                        <title> GENE LIST OF THE CHROMOSOME {w2} that starts at {t2} and ends at {z2} </title>
+                                        </head>
+                                        <body style="background-color: lightblue;"> '''
+                            for element in console:
+                                output += f'<p> - {element["external_name"]}</p>'
+
+                            output += '''<a href="/">Main page</a>
+                                         </body>
+                                         </html>'''
+                    except IndexError:
+                        output = Path('errorf.html').read_text()
 
                 except ValueError:
                     output = Path('errorf.html').read_text()
